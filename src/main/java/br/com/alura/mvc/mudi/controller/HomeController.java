f2a6e5924b142709d32aa667ae61a007b6e023e5
Jonatas <jnsfoliveira@gmail.com>
2021-01-02 14:17:56 -0300

    integrando com o Spring Data

--- src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -1,28 +1,25 @@
 package br.com.alura.mvc.mudi.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import br.com.alura.mvc.mudi.model.Pedido;
+import br.com.alura.mvc.mudi.repository.PedidoRepository;
 
 @Controller
 public class HomeController {
 
+	@Autowired
+	private PedidoRepository pedidoRepository;
+
 	@GetMapping("/home")
 	public String home(Model model) {
-		Pedido pedido = new Pedido();
-		pedido.setNomeProduto("Xiaomi Redmi 9");
-		pedido.setUrlImagem("https://images-na.ssl-images-amazon.com/images/I/6147ZT4PeFL._AC_SL1000_.jpg");
-		pedido.setUrlProduto("https://www.amazon.com.br/Celular-Xiaomi-Redmi-64GB-Dual/dp/B08B3VCF9G/ref=sr_1_2?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&dchild=1&keywords=xiamoi&qid=1609427214&sr=8-2");
-		pedido.setDescricao("Uma descricao qualquer para esse pedido");
-		
-		List<Pedido> pedidos = Arrays.asList(pedido);
+		List<Pedido> pedidos = pedidoRepository.findAll();
 		model.addAttribute("pedidos", pedidos);
-		
 		return "home";
 	}
 
